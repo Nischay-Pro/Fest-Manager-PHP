ab3410a71d2f03b04131afe35b286277c891798a
Rohitt Vashishtha <aero31aero@gmail.com>
2016-09-21 05:33:02 +0530

    As HRPNOM once said,'Some people aren't meant to code.'

--- src/PORTAL/Register/index.php
+++ src/PORTAL/Register/index.php
@@ -1,14 +1,14 @@
 <?php
-  session_start();
-  include("../functions/functions.php");
-  if(!isset($_SESSION['team_id'])){
-    echo "<script>window.open('login.php','_self')</script>";
-  }
-  else{
-    $team_id=$_SESSION['team_id'];
-    $team_collection=mysqli_query($con,"SELECT * FROM dosh_credentials WHERE team_id='$team_id'");
-    $result=mysqli_fetch_array($team_collection);
-  }
+session_start();
+include("../functions/functions.php");
+if(!isset($_SESSION['team_id'])){
+echo "<script>window.open('login.php','_self')</script>";
+}
+else{
+$team_id=$_SESSION['team_id'];
+$team_collection=mysqli_query($con,"SELECT * FROM dosh_credentials WHERE team_id='$team_id'");
+$result=mysqli_fetch_array($team_collection);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -46,43 +46,54 @@
       <form class="form-horizontal" action="workshopcall.php" role="form" method="GET" id="my-fucking-form">
         <input type="hidden" name="action" value="registerEvent">
         <div class="form-group">
-          <label  class="col-sm-2 control-label"
-          for="name">Workshop Name</label>
-          <div class="col-sm-10">
+          <label  class="col-sm-4 control-label"
+          for="name">Participant ID</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control"
             id="name" name="name" placeholder="Workshop Name"/>
           </div>
         </div>
         <div class="form-group">
-          <label class="col-sm-2 control-label"
-          for="room" >Room Number</label>
-          <div class="col-sm-10">
+          <label class="col-sm-4 control-label"
+          for="room" >Participant Name</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control"
             id="room" name="room" placeholder="Room Number"/>
           </div>
         </div>
         <div class="form-group">
-          <label class="col-sm-2 control-label"
-          for="cost" >Workshop Cost</label>
-          <div class="col-sm-10">
+          <label class="col-sm-4 control-label"
+          for="cost" >College Name</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control"
             id="cost" name="cost" placeholder="Cost"/>
           </div>
         </div>
         <div class="form-group">
-          <label class="col-sm-2 control-label"
-          for="time" >Workshop Time</label>
-          <div class="col-sm-10">
+          <label class="col-sm-4 control-label"
+          for="time" >Phone Number</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control"
             id="datetimepicker" name="time" placeholder="Workshop Time"/>
           </div>
         </div>
+        <div class="form-group">
+          <label class="col-sm-4 control-label"
+          for="time" >Email</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control"
+            id="datetimepicker" name="time" placeholder="Workshop Time"/>
+          </div>
+        </div>
+        <div class="form-group">
+            <button type="button" class="btn btn-lg btn-primary col-sm-4" name="register">Clear</button>
+            <button type="button" class="btn btn-lg btn-success col-sm-8" name="register" >Add Participant</button>
+        </div>
       </form>
-      <input class="btn btn-lg btn-success col-sm-12" type="submit" value="Add Workshop" name="register" >
-  </div>
-  
-</div>
-<script type="text/javascript">
     </div>
-  </body>
+    
+  </div>
+  <script type="text/javascript">
+</div>
+</body>
 </html>
